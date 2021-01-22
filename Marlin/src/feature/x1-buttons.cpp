0d597b1622fb95ce7e845f1fae8208877265d0a3
Leonardo Robol <leo@robol.it>
2021-01-22 09:24:19 +0100

    Implemented leveling mode.

--- Marlin/src/feature/x1-buttons.cpp
+++ Marlin/src/feature/x1-buttons.cpp
@@ -16,11 +16,20 @@
 #define HOMEK_PIN 30
 #define HOMEK_LED_PIN 17
 
-PGMSTR(X1_PREHEAT_STR,  "M104 S220");
+#define X1_PREHEAT_TEMP 220
+#define X1_PREHEAT_TEMP_STR "220"
+
+PGMSTR(X1_PREHEAT_STR,  "M104 S" X1_PREHEAT_TEMP_STR);
 PGMSTR(X1_PREHEAT_STOP, "M104 S0");
 PGMSTR(X1_RELATIVE_EXTRUDER_STR, "M83");
-PGMSTR(X1_FEED_STR, "G1 E5 F900");
-PGMSTR(X1_RETRACT_STR, "G1 E-5 F900");
+PGMSTR(X1_FEED_STR, "G1 E1 F600");
+PGMSTR(X1_RETRACT_STR, "G1 E-1 F600");
+PGMSTR(X1_G90_STR, "G90");
+
+PGMSTR(X1_LEVELING_0, "G0 X25 Y25 Z0 F1400");
+PGMSTR(X1_LEVELING_1, "G0 X75 Y25 Z0 F1400");
+PGMSTR(X1_LEVELING_2, "G0 X75 Y75 Z0 F1400");
+PGMSTR(X1_LEVELING_3, "G0 X25 Y75 Z0 F1400");
 
 #define X1_BLINK_PERIOD 500
 #define X1_FAST_BLINK_PERIOD 75
@@ -38,13 +47,19 @@ static short x1_plusk_led_state  = HIGH;
 static short x1_minusk_led_state = HIGH;
 static short x1_homek_led_state  = HIGH;
 
+// This is used to track the current leveling position. 0 refers
+// to the home location, and they are indexed from 0 to 3 in 
+// counter-clockwise order. 
+static short x1_leveling_pos = 0;
+
 typedef enum {
   IDLE,
   PRINTING,
   PREHEATING_FEED,
   PREHEATING_RETRACT,
-  FEED,
-  RETRACT,
+  FEEDING,
+  RETRACTING,
+  LEVELING
 } x1_state_t;
 
 static x1_state_t x1_state;
@@ -156,7 +171,7 @@ void x1_leds(const millis_t now) {
       x1_toggle_home_led(HIGH);
       break;
 
-    case FEED:
+    case FEEDING:
       x1_toggle_start_led(LOW);
       if (need_blink) {
         x1_toggle_plus_led();
@@ -173,7 +188,7 @@ void x1_leds(const millis_t now) {
       x1_toggle_home_led(HIGH);
       break;
 
-    case RETRACT:
+    case RETRACTING:
       x1_toggle_start_led(LOW);
       x1_toggle_plus_led(HIGH);
       if (need_blink) {
@@ -181,12 +196,51 @@ void x1_leds(const millis_t now) {
       }
       x1_toggle_home_led(HIGH);
       break;
+
+    case LEVELING:
+      x1_toggle_start_led(LOW);
+      x1_toggle_plus_led(HIGH);
+      x1_toggle_minus_led(HIGH);
+      x1_toggle_home_led(LOW);
+      break;
+  }
+}
+
+void x1_leveling_move() {
+  if (x1_state == LEVELING) {
+    switch (x1_leveling_pos) {
+      case 0:
+        queue.enqueue_one_P(X1_LEVELING_0);
+        break;
+      case 1:
+        queue.enqueue_one_P(X1_LEVELING_1);
+        break;
+      case 2:
+        queue.enqueue_one_P(X1_LEVELING_2);
+        break;
+      case 3:
+        queue.enqueue_one_P(X1_LEVELING_3);
+        break;  
+    }
   }
 }
 
 void x1_home_key_pressed() {
-  if (x1_state == IDLE)
-    queue.enqueue_now_P(G28_STR);
+  switch (x1_state) {
+    case IDLE:
+      queue.enqueue_now_P(G28_STR);
+      queue.enqueue_now_P(X1_G90_STR);
+      x1_state = LEVELING;
+      break;
+
+    case LEVELING:
+      queue.enqueue_now_P(G28_STR);
+      x1_state = IDLE;
+      break;
+
+    default:
+      break;
+  } 
 }
 
 void x1_plus_key_pressed() {
@@ -197,10 +251,17 @@ void x1_plus_key_pressed() {
       break;
 
     case PREHEATING_FEED:
-    case FEED:
+    case FEEDING:
       // Turn off heater, and change state to generic
       x1_state = IDLE;
+      quickstop_stepper();
       queue.enqueue_now_P(X1_PREHEAT_STOP);
+      break;
+
+    case LEVELING:
+      // Move to the next point
+      x1_leveling_pos = (x1_leveling_pos + 1) % 4;
+      x1_leveling_move();
       break;
 
     default:
@@ -216,11 +277,18 @@ void x1_minus_key_pressed() {
       break;
 
     case PREHEATING_RETRACT:
-    case RETRACT:
+    case RETRACTING:
       // Turn off heater, and change state to generic
       x1_state = IDLE;
+      quickstop_stepper();
       queue.enqueue_now_P(X1_PREHEAT_STOP);
       break;
+
+    case LEVELING:
+      // Move to the previous point
+      x1_leveling_pos = (x1_leveling_pos + 5) % 4;
+      x1_leveling_move();
+      break;      
 
     default:
       break;
@@ -268,9 +336,11 @@ void x1_buttons(millis_t now) {
 }
 
 void x1_switch_state(millis_t now) {
+  bool printing = printingIsActive();
+
   // If a print operation has started we stop messing with
   // the printer, and go back to the generic state. 
-  if (printingIsActive()) {
+  if (printing) {
     x1_state = PRINTING;
     return;
   }
@@ -278,26 +348,26 @@ void x1_switch_state(millis_t now) {
   switch (x1_state) {
     case PREHEATING_FEED:
       // Check if we have reached the desired temperature
-      if (thermalManager.degHotend(0) >= 219) {
+      if (thermalManager.degHotend(0) >= X1_PREHEAT_TEMP - 1) {
         queue.enqueue_one_P(X1_RELATIVE_EXTRUDER_STR);
-        x1_state = FEED;
+        x1_state = FEEDING;
       }
       break;
     case PREHEATING_RETRACT:
       // Check if we have reached the desired temperature
-      if (thermalManager.degHotend(0) >= 219) {
+      if (thermalManager.degHotend(0) >= X1_PREHEAT_TEMP - 1) {
         queue.enqueue_one_P(X1_RELATIVE_EXTRUDER_STR);
 
         // Before retracting we do a short feed move, which
         // helps with truncating the filament. 
         queue.enqueue_one_P(X1_FEED_STR);
         
-        x1_state = RETRACT;
+        x1_state = RETRACTING;
       }
       break;
 
     case PRINTING:
-      if (! printingIsActive()) {
+      if (! printing) {
         x1_state = IDLE;
       }
       break;
@@ -309,14 +379,14 @@ void x1_switch_state(millis_t now) {
 
 void x1_feed() {
   switch (x1_state) {
-    case FEED:
+    case FEEDING:
       // If we are about to end the commands in the queue, then 
       // push a few more extrusion ones. 
       if (! queue.has_commands_queued()) {
         queue.enqueue_one_P(X1_FEED_STR);
       }
       break;
-    case RETRACT:
+    case RETRACTING:
       // If we are about to end the commands in the queue, then 
       // push a few more extrusion ones. 
       if (! queue.has_commands_queued()) {
